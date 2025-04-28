11a210ce26e36ecf9a6bf326ca7b767fa83b8c4c
NickolayKu <nickolay135000@yandex.ru>
2025-04-29 01:29:45 +0300

    Fixed ts naming problems

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-quer
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "@/pages/Index";
 import NotFound from "@/pages/NotFound";
-import Start from "@/pages/start";
+import Start from "@/pages/Start";
 import Init from '@/pages/Init';
 
 const queryClient = new QueryClient();
